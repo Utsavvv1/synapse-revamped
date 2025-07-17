4367b4be82d1a10a91e5cd1fbfe89d6dea503946
anomitroid <anomitroid2005@gmail.com>
2025-07-17 11:41:10 +0000

    removed /target/ binariesp

--- main-logic/target/debug/deps/main_logic-1604e3f1e301a071.d
+++
@@ -1,5 +0,0 @@
-C:\Users\utsav\OneDrive\Desktop\Sem 4\synapse-revamped\main-logic\target\debug\deps\libmain_logic-1604e3f1e301a071.rmeta: src\main.rs
-
-C:\Users\utsav\OneDrive\Desktop\Sem 4\synapse-revamped\main-logic\target\debug\deps\main_logic-1604e3f1e301a071.d: src\main.rs
-
-src\main.rs:
