590787ee1d38fe789b683cb3ffb8413e17b3cf0c
anomitroid <anomitroid2005@gmail.com>
2025-07-17 11:13:53 +0000

    added minimal logic to detect foreground process on windows

--- main-logic/src/main.rs
+++ main-logic/src/main.rs
@@ -1,3 +1,15 @@
+mod monitor;
+
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        if let Some(app) = monitor::get_foreground_process_name() {
+            println!("Foreground process: {app}");
+        } else {
+            println!("Could not detect foreground process.");
+        }
+        sleep(Duration::from_secs(2));
+    }
 }
